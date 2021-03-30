c1f8e58f98f60af6afce808263b7a7b466ea474c
polyrain <jenna.macdonald@uqconnect.edu.au>
2021-03-30 20:37:31 +1000

    changes

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
+    
+    int abc = 1; // Unused
 
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
